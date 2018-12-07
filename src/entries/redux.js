cdb6dbf88a4bb967af63125e2317b96fe64e7741
Katherine Aguilera <katherinam35@gmail.com>
2018-12-07 13:53:46 -0500

    Agregando un Sore, Imprimiendo datos del estado

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -1,28 +1,48 @@
-// Enviar datos del input al la consola*/
+// importar metodo de redux
+import { createStore } from 'redux';
+
 const $form = document.getElementById('form');
 $form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
-  //no recargar la pagina
   event.preventDefault();
-  // tomar los datos del form FormData es una api del navegador
-  //que recibira un elemento del DOM esto hara que guarde los datos del formulario
   const data = new FormData($form);
-  // // data anterior.metodoget de FromData <'title'> mi name del input del form en este caso title')
   const title = data.get('title');
   console.log(title);
 }
-// //////////////////////////////////////////
-
-// // Ejemplo con arrow function
-// // Nota: cuando se declara una funcion como variable o como el const arrow function
-// // primero se debe declarar esa variable de la funcion y luego llamarla
-// const $form = document.getElementById('form');
-// const handleSubmit = event => {
-//   event.preventDefault();
-//   const data = new FormData($form);
-//   const title = data.get('title');
-//   console.log(title);
-// }
-
-// $form.addEventListener('submit', handleSubmit);
+//lista objetos, variablesmapas etc
+const initialState = [
+  {
+    "title": "Kurenai",
+  },
+  {
+    "title": "Imagine",
+  },
+  {
+    "title": "Whit out you",
+  }
+]
+const store = createStore(
+  // Lo que se pasa en un store 3 cosas//
+  //1. reducer = funcion pura
+  (state) => state,
+  // 2.Estado inicial puede ser el modelado de mis datos inicial initialstate = variable
+  initialState,
+  // 3.Enhancer = conexion con otras herramienta ejem webtools de redux
+  // cod segun este repo https://github.com/zalmoxisus/redux-devtools-extension
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+)
+// de redux.html
+const $container = document.getElementById('playlist');
+const playlist = store.getState();
+// Imprimir la iteracion de los datos
+playlist.forEach((item) => {
+  // crear etiqueta
+  const template = document.createElement('p');
+  // textContent es una propiedad recibira el title de initialState
+  template.textContent = item.title;
+  // appenChild añadir nuevos hijos html
+  $container.appendChild(template);
+})
+// Mostar en consola los datos
+console.log(store.getState())
