217ec39707bf88d510001eaa41ffe27aac5851c0
Katherine Aguilera <katherinam35@gmail.com>
2018-12-10 21:04:09 -0500

    Instalar react-redux, Agregando Store

--- src/entries/home.js
+++ src/entries/home.js
@@ -4,8 +4,27 @@ import HomePage from '../pages/containers/homepage';
 // import Playlist from './src/playlist/components/playlist';
 import data from '../api.json';
 
-const homeContainer = document.getElementById('home-container')
+import { createStore } from 'redux';
+/****REDUX ***************************************************************/
+// inicializar estado
+const initialState = {
+  // datos de la api
+  data: {
+    // ...descomponer es decir obtener todos los datos que hay dentro de data
+    ...data
+  }
+}
+// crear el store con sus tres parametros
+const store = createStore(
+  (state) => state,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
+console.log(store.getState());
+/** **********************************************************************/
+
+const homeContainer = document.getElementById('home-container')
 // ReactDOM.render(que voy a renderizar, donde lo haré);
 // const holaMundo = <h1>hola Estudiante!</h1>;
 render( <HomePage data={data}/>, homeContainer);
